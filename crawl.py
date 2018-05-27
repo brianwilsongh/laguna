b24dc0e56799a9750bc6e5a9bb5d2c4f787e95ba
wilsontheory <wilson.brian90@gmail.com>
2018-05-27 17:14:34 -0500

    Start to add RSS scraping

--- crawl.py
+++ crawl.py
@@ -2,32 +2,25 @@ from splinter import Browser
 import numpy as np
 import utils
 
-urls = []
-urls.append("https://www.cnbc.com/2018/05/24/crowdstrikes-security-platform-could-someday-attract-amazon-google.html")
-urls.append("http://www.marketwatch.com")
-urls.append("http://money.cnn.com/")
-urls.append("https://www.marketwatch.com/story/after-earnings-barrage-and-data-stock-market-moves-now-hinge-on-global-trade-2018-05-05")
-urls.append("https://finance.yahoo.com/")
-urls.append("https://stackoverflow.com/questions/4990718/python-about-catching-any-exception")
-urls.append('https://webonastick.com/php.html')
-laguna_map = {}
-# with Browser('firefox', headless=True) as browser:
-try:
-    browser = Browser('firefox', headless=True)
-    for url in urls:
-        try:
-            print(f'visiting {url}')
-            browser.visit(url)
-            print(f'processing page of {url}')
-            laguna_map[url] = utils.analyze_text(utils.process_page(browser.html))
-        except Exception as e:
-            print("Error!")
-            print(e)
-            laguna_map[url] = None
-except Exception as e:
-    print(e)
-finally:
-    browser.quit()
-    for url, features in laguna_map.items():
-        print(url)
-        if features != None: print(f"Polarity:{features[0]}  Subjectivity:{features[1]}")
+def init(urls):
+    laguna_map = {}
+    # with Browser('firefox', headless=True) as browser:
+    try:
+        browser = Browser('firefox', headless=True)
+        for url in urls:
+            try:
+                print(f'visiting {url}')
+                browser.visit(url)
+                print(f'processing page of {url}')
+                laguna_map[url] = utils.analyze_text(utils.process_page(browser.html))
+            except Exception as e:
+                print("Error!")
+                print(e)
+                laguna_map[url] = None
+    except Exception as e:
+        print(e)
+    finally:
+        browser.quit()
+        for url, features in laguna_map.items():
+            print(url)
+            if features != None: print(f"Polarity:{features[0]}  Subjectivity:{features[1]}")
--- initiator.py
+++ initiator.py
@@ -1,19 +1,36 @@
 import feedparser
 import json
 from newspaper import Article
+from datetime import datetime
+import utils
 
 ticker_symbols = ['TGT', 'XLE', 'WMT']
 
+aggregate_data = {}
+
 for symbol in ticker_symbols:
-    rss = f'http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol={symbol}'
-    data = feedparser.parse(rss)
-    for entry in data.entries:
-        print(entry.published_parsed)
-        print(entry.link)
-        article = Article(entry.link)
-        article.download()
-        article.parse()
-        print(article.authors)
-        print(article.publish_date)
-        print(article.text)
-        print("\n\n")
+    aggregate_data[symbol] = {}
+
+    rss_feeds = []
+    rss_feeds.append(f'http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol={symbol}')
+    for rss_feed in rss_feeds:
+        try:
+            for entry in feedparser.parse(rss_feed).entries:
+                print(entry.published_parsed)
+                print(entry.link)
+
+                if not entry.link in aggregate_data[symbol]:
+                    article = Article(entry.link)
+                    article.download()
+                    article.parse()
+                    aggregate_data[symbol][entry.link] = utils.analyze_text(article.text)
+                    print(article.authors)
+                    print(article.publish_date.date())
+                    print(datetime.today().date())
+                    print(f'equal? {datetime.today().date() == article.publish_date.date()}')
+                    print("\n\n")
+                    print(aggregate_data)
+        except Exception as e:
+            print(e)
+            print("Moving to next...")
+            continue
